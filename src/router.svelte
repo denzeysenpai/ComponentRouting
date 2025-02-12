907334ef5c7481646d6e17e5454e06700e562200
DenzeySenpai <marksolante221@gmail.com>
2025-02-12 13:12:21 +0800

    [UPDATE 0.0.2 : Revise]

--- src/router.svelte
+++ src/router.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-    import { CurrentRoute } from "./router.svelte.ts";
-    let {routes} = $props()
+	import { onMount } from 'svelte';
+	import { CurrentRoute } from './router.svelte.ts';
+	let { routes } = $props();
 
-    onMount(() => {
-        let url = window.location.href
-        const switchOnMatch = (path: string) => {
-            if (url.includes(path)) CurrentRoute.SwitchTo(path)
-        }
-        routes.forEach((element : any) => {
-            switchOnMatch(element.path)
-        });
-    })
+	onMount(() => {
+		let url = window.location.href;
+		const switchOnMatch = (path: string) => {
+			if (url.includes(path)) CurrentRoute.SwitchTo(path);
+		};
+		routes.forEach((element: any) => {
+			switchOnMatch(element.path);
+		});
+	});
 </script>
 
 {#each routes as route}
-{#if CurrentRoute.MatchWithCurrent(route.path)}
-<route.component/>
-{/if}
+	{#if CurrentRoute.MatchWithCurrent(route.path)}
+		<route.component />
+	{/if}
 {/each}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,59 @@
 <script lang="ts">
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 
-    // import {Router, Route, navigate} from "svelte-routing"
-    import Router from "../router.svelte"
-	import { CurrentRoute } from "../router.svelte.ts";
-    import Page1 from "../views/page1/page1.svelte"
-    import Page2 from "../views/page2/page2.svelte"
-    import Page3 from "../views/page3/page3.svelte"
-	import { goto } from "$app/navigation";
+	// import {Router, Route, navigate} from "svelte-routing"
+	import Router from '../router.svelte';
+	import { CurrentRoute } from '../router.svelte.ts';
 
-    const routes = [
-        {
-            path: "page-1",
-            component: Page1
-        },
-        {
-            path: "",
-            component: Page1
-        },
-        {
-            path: "page-2",
-            component: Page2
-        },
-        {
-            path: "page-3",
-            component: Page3
-        },
-    ]
+	// import your routes as components
+	import Page1 from '../views/page1/page1.svelte';
+	import Page2 from '../views/page2/page2.svelte';
+	import Page3 from '../views/page3/page3.svelte';
+	import { goto } from '$app/navigation';
 
-    onMount(()=>{
-        let curr = CurrentRoute
-        console.log("MOUNTED")
-        if(!window.location.href.includes("/#/")) goto("/#/")
-    })
+	const routes = [
+		{
+			path: 'page-1',
+			component: Page1
+		},
+		{
+			path: '',
+			component: Page1
+		},
+		{
+			path: 'page-2',
+			component: Page2
+		},
+		{
+			path: 'page-3',
+			component: Page3
+		}
+	];
 
+	onMount(() => {
+		let curr = CurrentRoute;
+		console.log('MOUNTED');
+		if (!window.location.href.includes('/#/')) goto('/#/');
+	});
 </script>
-<a href="/#/page-1" onclick={()=>{
-    CurrentRoute.SwitchTo("page-1")
-}}>page 1</a>
-<a href="/#/page-2" onclick={()=>{
-    CurrentRoute.SwitchTo("page-2")
-}}>page 2</a>
-<a href="/#/page-3" onclick={()=>{
-    CurrentRoute.SwitchTo("page-3")
-}}>page 3</a>
 
-<Router routes={routes}/>
+<a
+	href="/#/page-1"
+	onclick={() => {
+		CurrentRoute.SwitchTo('page-1');
+	}}>page 1</a
+>
+<a
+	href="/#/page-2"
+	onclick={() => {
+		CurrentRoute.SwitchTo('page-2');
+	}}>page 2</a
+>
+<a
+	href="/#/page-3"
+	onclick={() => {
+		CurrentRoute.SwitchTo('page-3');
+	}}>page 3</a
+>
+
+<Router {routes} />
